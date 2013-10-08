dea1fdba9c7146b3da4e7c02f79dba7d0a8307a2
Giovanni Capuano <webmaster@giovannicapuano.net>
2013-10-08 16:15:20 +0200

    Delete a.bat

--- a.bat
+++
@@ -1,1 +0,0 @@
-gem uninstall image-dumper && gem build E:\Users\Giovanni\Documents\GitHub\Ruby\dumper\dumper.gemspec && gem install E:\Users\Giovanni\Documents\GitHub\Ruby\dumper\image-dumper-0.5.3.gem
