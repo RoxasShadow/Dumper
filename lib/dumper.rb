143a60539c379e8a040f867f933d6732dd59d759
Giovanni Capuano <webmaster@giovannicapuano.net>
2014-01-29 22:26:09 +0100

    Add LICENSE

--- lib/dumper.rb
+++ lib/dumper.rb
@@ -3,18 +3,18 @@
 #
 # This file is part of Dumper.
 #
-# Smogon-API is free software: you can redistribute it and/or modify
+# Dumper is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Smogon-API is distributed in the hope that it will be useful,
+# Dumper is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Smogon-API.  If not, see <http://www.gnu.org/licenses/>.
+# along with Dumper.  If not, see <http://www.gnu.org/licenses/>.
 #++
 
 require 'open-uri'
